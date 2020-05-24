5292b952f74303da0a9fe002917684df5f260740
Ben Brunyee <brunyeeb@gmail.com>
2020-05-24 20:34:22 +0100

    Converting to requests rather than puppeteer

--- server.js
+++ server.js
@@ -26,20 +26,6 @@ app.set('json spaces', 4);
 // Server calls
 //------------------------------------------------------------------------------------//
 
-// async function getUrl() {
-//   var result = await fetch("https://www.google.co.uk/search?q=gym&num=10")
-//   var html = await result.text();
-//   var $ = cheerio.load(html);
-//   var titles = $("div[role=heading]").parent().parent().find("a");
-//   titles.each((index, elem) => {
-//     console.log($(elem).attr("href").replace(/^.*?q=/, "").replace(/((\.com)|(\.co\.uk)|(\.org(\.uk)?)|(\.uk)|(\.co)).*/, "$1"));
-//   });
-//   console.log("Done");
-// }
-
-// getUrl();
-
-
 app.get("/", async (req, res) => {
   var jsonResponse = {status: "success", body: {results: []}};
   var searchTerm = req.query.searchTerm;
@@ -47,45 +33,44 @@ app.get("/", async (req, res) => {
   var num = req.query.num;
 
   if (!searchTerm) {
-    res.json({status: "failed", body: {}});
+    res.json({status: "failed", body: {}, msg: "Provide a search term with 'searchTerm=query'"});
     return;
   }
 
-  var googleSearchString = `https://www.google.com/search?q=${searchTerm}`;
+  var googleSearchString = `https://www.google.com/search?q=${searchTerm}&num=100`;
 
   if (time === "d" || time === "w" || time === "m") {   // Show google indexes within the past 24 hours | 7 days | 1 month
     googleSearchString += `&as_qdr=${time}`;
-  }
-
-  if (num) {
-    googleSearchString += `&num=${num}`;
-  }
-  else {
-    googleSearchString += `&num=100`;   // By default return the max of 100 results
   }
 
   var links = [];
   try {
     links = await SearchGoogle(googleSearchString);
     if (links.status === "failed" || !links.body) {
-      res.json({status: "failed", body: {}});
+      res.json({status: "failed", body: {results: [], msg: "Failed when trying to get Google Searches"}});
       return;
     }
   }
   catch (err) {
     console.log(err);
-    res.json({status: "failed", body: {}});
+    res.json({status: "failed", body: {results: [], msg: "Failed when trying to get Google Searches"}});
     return;
   }
 
   links = GetUnique(links.body);
+  if (num <= 100) {
+    links = links.slice(0, num);
+  }
+  else {
+    res.json({status: "failed", body: {results: [], msg: "Cannot return more than 100 websites"}})
+  }
 
   for (var link of links) {
     try {
       console.log(`Getting info on: ${link}`);
       var result = await CreateJsonInfo(link)
       if (result === "failed" || !result.body) {
-        res.json({status: "failed", body: {}});
+        res.json({status: "failed", body: {results: [], msg: "Failed when creating/finding JSON info for results"}});
         return;
       }
       jsonResponse.body.results.push(result.body);
@@ -113,30 +98,23 @@ Gets results from a google search. This will return a list of links bases on the
 results with the provided options. The links returned are sorted in order of the search
 results but are not weeded for duplicates.
 */
-function SearchGoogle(url) {
+function SearchGoogle(link) {
   return new Promise(async (resolve, reject) => {
     var jsonResponse = {status: "success", body: {}};
 
-    const browser = await puppet.launch();
-    const page = await browser.newPage();
-    await page.goto(url).catch(err => {
-      console.log(err);
-      return({status: "failed", body: {}});
-    });
+    var html = await (await fetch(link)).text();
+    var $ = cheerio.load(html);
 
-    var results = await page.$x('//h3/../../a');
     var links = [];
-    if (results) {
-      for (var elem of results) {
-        var link = await elem.getProperty("href");
-        link = await link.jsonValue();
-        if (link !== undefined && link != "") {
-          links.push(link);
-        }
-      };
-    }
 
-    browser.close();
+    var results = $("div[role=heading]").parent().parent().find("a");   // Get the results from the page
+    results.each((i, elem) => {
+      var href = $(elem).attr("href");
+      if (href) {
+        href = href.replace(/^.*?q=/, "");    // Replace the rubbish at the beginning
+        links.push(href);
+      }
+    });
 
     jsonResponse.body = links;
     resolve(jsonResponse);
@@ -182,62 +160,84 @@ function CreateJsonInfo(link) {
       contactPage: {}
     };
 
-    const browser = await puppet.launch();
-    const page = await browser.newPage();
-    await page.goto(link);    // Get HTML of link
+    // const browser = await puppet.launch();
+    // const page = await browser.newPage();
+    // await page.goto(link);    // Get HTML of link
+
+    // Get HTML of website
+    var html = await (await fetch(link)).text();
+    var $ = cheerio.load(html);
+
 
     // Get Facebook details
-    var [facebook] = await page.$x('//a[contains(@href, "facebook.com") and not(contains(@href, "sharer"))]');
+    var facebook = ($("a[href]").filter((i, elem) => {
+        return $(elem).attr("href").match(/facebook\.com\/?(?!sharer)/);
+    })).first();
+
     if (facebook) {
-      var href = await (await facebook.getProperty("href")).jsonValue();
-      json[identifier].facebookPage.link = href;
-      json[identifier].facebookPage.status = "success";
+      var href = facebook.attr("href");
+      if (href) {
+        json[identifier].facebookPage.link = href.replace(/(\.com\/.*?)\/.*/, "$1");    // Replace everything after the identifier
+        json[identifier].facebookPage.status = "success";
+      }
     }
 
     // Get Twitter details
-    var [twitter] = await page.$x('//a[contains(@href, "twitter.com") and not(contains(@href, "intent"))]');
+    var twitter = ($("a[href]").filter((i, elem) => {
+      return $(elem).attr("href").match(/twitter\.com\/?(?!intent)/);
+    })).first();
+
     if (twitter) {
-      var href = await (await twitter.getProperty("href")).jsonValue();
-      json[identifier].twitterPage.link = href;
-      json[identifier].twitterPage.status = "success"
+      var href = twitter.attr("href");
+      if (href) {
+        json[identifier].twitterPage.link = href.replace(/(\.com\/.*?)\/.*/, "$1");   // Replace everything after the identifier
+        json[identifier].twitterPage.status = "success"
+      }
     }
+
+    // var [twitter] = await page.$x('//a[contains(@href, "twitter.com") and not(contains(@href, "intent"))]');
+    // if (twitter) {
+    //   var href = await (await twitter.getProperty("href")).jsonValue();
+    //   json[identifier].twitterPage.link = href;
+    //   json[identifier].twitterPage.status = "success"
+    // }
 
     // Get Instagram details
-    var [instagram] = await page.$x('//a[contains(@href, "instagram.com")]');
-    if (instagram) {
-      var href = await (await instagram.getProperty("href")).jsonValue();
-      json[identifier].instagramPage.link = href;
-      json[identifier].instagramPage.status = "success";
-    }
+    // var [instagram] = await page.$x('//a[contains(@href, "instagram.com")]');
+    // if (instagram) {
+    //   var href = await (await instagram.getProperty("href")).jsonValue();
+    //   json[identifier].instagramPage.link = href;
+    //   json[identifier].instagramPage.status = "success";
+    // }
 
-    // Get contact details
-    var contactPageLinks = await page.$x('//a');
-    var contactPage;
-    for (var tempPage of contactPageLinks) {
-      try {
-        var tempLink = (await (await tempPage.getProperty("href")).jsonValue()).replace(/((.*?)\/\/(.*?)\/)|^\//, "");
-        if (tempLink.match(/contact/i)) {
-          contactPage = tempLink;
-        }
-      }
-      catch (err) {}
-    }
+    // // Get contact details
+    // var contactPageLinks = await page.$x('//a');
+    // var contactPage;
+    // for (var tempPage of contactPageLinks) {
+    //   try {
+    //     var tempLink = (await (await tempPage.getProperty("href")).jsonValue()).replace(/((.*?)\/\/(.*?)\/)|^\//, "");
+    //     if (tempLink.match(/contact/i)) {
+    //       contactPage = tempLink;
+    //     }
+    //   }
+    //   catch (err) {}
+    // }
 
-    if (contactPage) {
-      contactPage = `${link}/${contactPage}`;
-      json[identifier].contactPage.link = contactPage;
-      try {
-        var contactInfo = await GetContactInfo(contactPage);
-        if (contactInfo.status === "failed" || !contactInfo.body) {
-          return ({status: "failed", body: {}});
-        }
-        json[identifier].contactPage = {...json[identifier].contactPage, ...contactInfo.body};
-      }
-      catch (err) {
-        console.log(err);
-        return({status: "failed", body: {}});
-      }
-    }
+    // if (contactPage) {
+    //   contactPage = `${link}/${contactPage}`;
+    //   json[identifier].contactPage.link = contactPage;
+    //   try {
+    //     var contactInfo = await GetContactInfo(contactPage);
+    //     if (contactInfo.status === "failed" || !contactInfo.body) {
+    //       return ({status: "failed", body: {}});
+    //     }
+    //     json[identifier].contactPage = {...json[identifier].contactPage, ...contactInfo.body};
+    //   }
+    //   catch (err) {
+    //     console.log(err);
+    //     return({status: "failed", body: {}});
+    //   }
+    // }
 
     jsonResponse.body =  json;
     resolve(jsonResponse);
