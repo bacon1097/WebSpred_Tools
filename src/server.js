2dd50d04b0c344c1cbbe55d0b107dd74fb8022ee
Ben Brunyee <brunyeeb@gmail.com>
2020-05-28 22:10:04 +0100

    Netlify fix

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@
 // Modules
 //------------------------------------------------------------------------------------//
 
+'use strict';
 const express = require('express');
 const cors = require('cors');
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const fs = require('fs');
 const serverless = require('serverless-http');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 //------------------------------------------------------------------------------------//
 // Config
@@ -29,14 +32,19 @@ app.set('json spaces', 4);
 // Server calls
 //------------------------------------------------------------------------------------//
 
-router.get("/", async (req, res) => {
-  res.json({
-    body: "Hello"
-  });
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
 });
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use("/.netlify/functions/server", router);
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
+module.exports = app;
 module.exports.handler = serverless(app);
 
 // app.get("/", async (req, res) => {
