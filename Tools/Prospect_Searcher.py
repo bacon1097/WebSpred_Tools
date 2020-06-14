8c4b750907e658ba517c4dba52872e06e687a270
Ben Brunyee <brunyeeb@gmail.com>
2020-06-14 15:00:27 +0100

    Added Options to save to xls file

--- Tools/Prospect_Searcher.py
+++ Tools/Prospect_Searcher.py
@@ -2,7 +2,7 @@
 # Modules
 #------------------------------------------------------------------------------------#
 
-import requests, argparse, re, json
+import requests, argparse, re, json, xlwt, os
 import logging as log
 from bs4 import BeautifulSoup
 from googlesearch import search
@@ -12,7 +12,7 @@ from googleapiclient.discovery import build
 # Variables & Config
 #------------------------------------------------------------------------------------#
 
-log.basicConfig(level=log.INFO, format="%(levelname)s : %(asctime)s : %(message)s", datefmt="%I:%M:%S %p")
+log.basicConfig(level=log.DEBUG, format="%(levelname)s : %(asctime)s : %(message)s", datefmt="%I:%M:%S %p")
 parser = argparse.ArgumentParser()
 parser.add_argument("--search", "-s", metavar="STRING", required=True, help="Enter a search term")
 parser.add_argument("--results", "-r", metavar="INT", required=False, type=int, default=4, help="How " +\
@@ -41,14 +41,161 @@ socialsFlag = args.socials
 #------------------------------------------------------------------------------------#
 
 def Main():
+  log.info(f"Getting google search results for {searchString}")
   links = GetGoogleResults(searchString, results, time)
   links = GetUnique(links)[:results]    # Only use the number of results requested
   infoArray = []
   for link in links:
+    log.info(f"Getting information on {link}")
     info = GetInfo(link)
     log.debug("Appending to array: " + str(info))
     infoArray.append(info)
   log.info(json.dumps(infoArray, indent=4))
+  while True:
+    response = int(input("What would you like to do with this information?\n" +
+      "1. Save to .xls file\n" +
+      "2. Save to .txt file\n" +
+      "3. Send emails\n" +
+      "4. Exit\n"))
+    if (response == 1):
+      ExportXls(infoArray)
+      break
+    elif (response == 2):
+      break
+    elif (response == 3):
+      break
+    elif (response == 4):
+      break
+    else:   # If response is not a valid response
+      continue
+
+#------------------------------------------------------------------------------------#
+# Export information to xls file
+#------------------------------------------------------------------------------------#
+
+def ExportXls(data, fileName="Prospects.xls"):
+  bold = xlwt.easyxf("font: bold on")
+
+  book = xlwt.Workbook()
+  sheet = book.add_sheet("Prospects")
+
+  # Titles
+  titles = [
+    "Prospect",
+    "Website",
+    "Contact Page",
+    "Contact Email",
+    "Contact Number",
+    "Facebook Page Link",
+    "Facebook Page Name",
+    "Facebook Likes",
+    "Facebook Followers",
+    "Instagram Page Link",
+    "Instagram Username",
+    "Instagram Followers",
+    "Instagram Following"
+  ]
+
+  # Write titles to file
+  counter = 0
+  for title in titles:
+    sheet.write(0, counter, title, bold)
+    sheet.col(counter).width = 6000
+    counter += 1
+
+  rowCounter = 1
+  na = "N/A"    # Identifier for empty data values
+  for prospect in data:   # Every loop = next row
+    companyName = list(prospect.keys())[0]
+    sheet.write(rowCounter, 0, companyName)
+
+    prospectData = prospect[companyName]
+    if (prospectData["result"] == "success"):
+      for key in prospectData:    # Every loop = different column
+        if (key == "website"):
+          sheet.write(rowCounter, 1, prospectData[key])
+        elif (key == "Contact Info"):
+          if (prospectData[key]["result"] == "success"):
+            if (prospectData[key]["link"]):
+              sheet.write(rowCounter, 2, prospectData[key]["link"])
+            else:
+              sheet.write(rowCounter, 2, na)
+
+            if (prospectData[key]["email"]):
+              sheet.write(rowCounter, 3, prospectData[key]["email"])
+            else:
+              sheet.write(rowCounter, 3, na)
+
+            if (prospectData[key]["number"]):
+              sheet.write(rowCounter, 4, prospectData[key]["number"])
+            else:
+              sheet.write(rowCounter, 4, na)
+          else:
+            for i in range(3):
+              counter = i + 2
+              sheet.write(rowCounter, counter, na)
+        elif (key == "Facebook Page"):
+          if (prospectData[key]["result"] == "success"):
+            if (prospectData[key]["link"]):
+              sheet.write(rowCounter, 5, prospectData[key]["link"])
+            else:
+              sheet.write(rowCounter, 5, na)
+
+            if (prospectData[key]["page name"]):
+              sheet.write(rowCounter, 6, prospectData[key]["page name"])
+            else:
+              sheet.write(rowCounter, 6, na)
+
+            if (prospectData[key]["likes"]):
+              sheet.write(rowCounter, 7, prospectData[key]["likes"])
+            else:
+              sheet.write(rowCounter, 7, na)
+
+            if (prospectData[key]["followers"]):
+              sheet.write(rowCounter, 8, prospectData[key]["followers"])
+            else:
+              sheet.write(rowCounter, 8, na)
+          else:
+            for i in range(4):
+              counter = i + 5
+              sheet.write(rowCounter, counter, na)
+        elif (key == "Instagram Page"):
+          if (prospectData[key]["result"] == "success"):
+            if (prospectData[key]["link"]):
+              sheet.write(rowCounter, 9, prospectData[key]["link"])
+            else:
+              sheet.write(rowCounter, 9, na)
+
+            if (prospectData[key]["username"]):
+              sheet.write(rowCounter, 10, prospectData[key]["username"])
+            else:
+              sheet.write(rowCounter, 10, na)
+
+            if (prospectData[key]["followers"]):
+              sheet.write(rowCounter, 11, prospectData[key]["followers"])
+            else:
+              sheet.write(rowCounter, 11, na)
+
+            if (prospectData[key]["following"]):
+              sheet.write(rowCounter, 12, prospectData[key]["following"])
+            else:
+              sheet.write(rowCounter, 12, na)
+          else:
+            for i in range(4):
+              counter = i + 9
+              sheet.write(rowCounter, counter, na)
+
+    else:
+      for i in range(13):
+        sheet.write(rowCounter, i, na)
+    rowCounter += 1
+
+  # Save file
+  try:
+    book.save(fileName)
+    log.info(f"{fileName} has been saved")
+  except Exception as e:
+    log.error(e)
 
 #------------------------------------------------------------------------------------#
 # Get websites from google
@@ -112,6 +259,7 @@ def GetInfo(link):
       info["Facebook Page"] = CheckFacebook(str(facebook[0]["href"]))
     else:
       info["Facebook Page"]["msg"] = "Could not find page"
+      info["Facebook Page"]["result"] = "failed"
 
     # Disabling Twitter as it has run into problems
     # Find twitter webpage
@@ -120,6 +268,7 @@ def GetInfo(link):
     #   info["Twitter Page"] = CheckTwitter(str(twitter[0]["href"]))
     # else:
     #   info["Twitter Page"]["msg"] = "Could not find page"
+    #   info["Twitter Page"]["result"] = "failed"
 
     # Find instagram webpage
     instagram = soup.find_all('a', href=re.compile(r"instagram\.com\/.*"))
@@ -127,6 +276,7 @@ def GetInfo(link):
       info["Instagram Page"] = CheckInsta(str(instagram[0]["href"]))
     else:
       info["Instagram Page"]["msg"] = "Could not find page"
+      info["Instagram Page"]["result"] = "failed"
 
   # Find contact page
   contactPage = soup.find_all('a', text=re.compile(r"contact", re.IGNORECASE))
